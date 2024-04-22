39254d2eb425a3821b7b03bfb24aa803fdfd4dad
kaka1104 <164013210+kaka1104@users.noreply.github.com>
2024-04-22 20:18:57 +0700

    midterm

--- OracleProjectMiedterm/obj/Debug/net8.0/OracleProjectMiedterm.AssemblyInfo.cs
+++ OracleProjectMiedterm/obj/Debug/net8.0/OracleProjectMiedterm.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OracleProjectMiedterm")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c4ecc51c93e01b4eb817213c55e005786355bf4")]
 [assembly: System.Reflection.AssemblyProductAttribute("OracleProjectMiedterm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OracleProjectMiedterm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
